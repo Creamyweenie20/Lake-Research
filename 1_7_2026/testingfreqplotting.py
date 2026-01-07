961fa177ee0cf8dd19a2d2fec28038ed9f296773
creamyweenie <kitroyfeeney@gmail.com>
2026-01-06 22:08:48 -0500

    PML fix

---
+++ 1_7_2026/testingfreqplotting.py
@@ -0,0 +1,13 @@
+import meep as mp
+import matplotlib.pyplot as plt
+import numpy as np 
+from IPython.display import Video 
+from meep import mpb
+from scipy.optimize import minimize
+import math
+import os
+import utils as u
+
+taper = u.tapering(380,180,14)
+Qsi, freqs, V = u.defectandmirrors(500, length = 337, xlength= 218, ylengths = taper, mirrorlength=382, excitation = mp.Hz, nummirrors=5, findModes=True,
+                                     modevisulization=True, smoothing = True, resolution = 16, showgeo = True, ModeVolume = True, dir = 'plotting', freqsolver = False)
